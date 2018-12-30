f701a49654ceb08e46d4ce709ed344d1fe99ce3f
Trent Boyd <trent.boyd@gmail.com>
2018-12-30 13:57:09 -0600

    add more borders, fonts, etc

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,14 @@ import "./App.scss";
 
 export default class App extends Component {
   render() {
-    return (
-      <TextWindow>
-        Hey bro. Did the sound wake you up? Were you freaked out?
-      </TextWindow>
-    );
+    const flavors = ["plain", "mint", "strawberry", "banana", "peanut"];
+
+    return flavors.map(flavor => (
+      <div style={{ marginBottom: 10 }}>
+        <TextWindow flavor={flavor} key={flavor}>
+          Hey bro. Did the sound wake you up? Were you freaked out?
+        </TextWindow>
+      </div>
+    ));
   }
 }
--- src/components/TextWindow/TextWindow.tsx
+++ src/components/TextWindow/TextWindow.tsx
@@ -3,20 +3,29 @@ import classnames from "classnames";
 
 import "./TextWindow.scss";
 
+// TODO: Get enums/types to work with these.
+// export type Flavors = "plain" | "mint" | "strawberry" | "banana" | "peanut";
+// export type Speeds = "fast" | "normal" | "slow";
+
+// type Props = {
+//   flavor?: Flavors;
+//   speed?: Speeds;
+// };
+
 type Props = {
-  flavor?: "plain" | "mint" | "strawberry" | "banana" | "peanut";
-  speed?: "fast" | "normal" | "slow";
+  flavor?: string;
+  speed?: string;
 };
 
 export default class TextWindow extends Component<Props> {
   static defaultProps = {
-    flavor: "plain", // not implemented
+    flavor: "plain",
     speed: "normal" // not implemented
   };
 
   render() {
     return (
-      <div className={classnames("window", this.props.flavor)}>
+      <div className={classnames("window", `flavor-${this.props.flavor}`)}>
         {this.props.children}
       </div>
     );
